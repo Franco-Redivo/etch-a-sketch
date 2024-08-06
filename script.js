937b31181893fec27b9c6adea6dff531659dc56e
Franco-Redivo <144297778+Franco-Redivo@users.noreply.github.com>
2024-08-06 20:07:21 -0300

    added user prompt to chose grid size and rgb colouring

--- script.js
+++ script.js
@@ -6,27 +6,42 @@ function appendToContainer(div){
     container.appendChild(div);
 }
 
+function random_rgb() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgb(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ')';
+}
+
+function paintSquare(div){
+    let color = random_rgb();
+    div.addEventListener("mouseenter",(e) => {
+        e.target.style.backgroundColor = color;
+    });
+}
+
 //creates each div and adds them to container
 function createGrid(gridSize){
     const itemSize = 768/gridSize;
     
-    for(let i = 0; i < 256; i++){
+    for(let i = 0; i < gridSize * gridSize; i++){
         const div = document.createElement("div");
         div.classList.add("square");
+        div.style.width = `${itemSize}px`;
+        div.style.height = `${itemSize}px`;
         appendToContainer(div);
         paintSquare(div);
     }
 }
 
 
-function paintSquare(div){
-    div.addEventListener("mouseenter",(e) => {
-        e.target.style.backgroundColor = "green";
-    });
-}
-
 const btn = document.querySelector("button");
 
 btn.addEventListener("click",() => {
     const option = prompt("Enter number of squares per side for new grid");
+    if(option > 100){
+        alert("100 is the limit");
+    }
+    container.innerHTML = '';
+    createGrid(option);
 })
+
+createGrid(16);
